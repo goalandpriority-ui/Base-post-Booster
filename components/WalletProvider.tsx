221bb689b2db7c56ec1ef10591329ccf4c3e1dc9
goalandpriority-ui <goalandpriority@gmail.com>
2026-02-25 13:44:58 +0530

    Update WalletProvider.tsx

--- components/WalletProvider.tsx
+++ components/WalletProvider.tsx
@@ -2,6 +2,8 @@
 
 import { WagmiProvider, createConfig, http } from "wagmi"
 import { base } from "wagmi/chains"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactNode } from "react"
 
 const config = createConfig({
   chains: [base],
@@ -10,6 +12,14 @@ const config = createConfig({
   }
 })
 
-export function WalletProvider({ children }: any) {
-  return <WagmiProvider config={config}>{children}</WagmiProvider>
+const queryClient = new QueryClient()
+
+export default function WalletProvider({ children }: { children: ReactNode }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+    </WagmiProvider>
+  )
 }
