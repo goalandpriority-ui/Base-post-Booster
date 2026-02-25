f8f717cb62b0b79a22a93f4688745eac10c66ad4
goalandpriority-ui <goalandpriority@gmail.com>
2026-02-25 12:47:00 +0530

    Create page.tsx

---
+++ app/leaderboard/page.tsx
@@ -0,0 +1,10 @@
+import LeaderboardTable from "@/components/LeaderboardTable"
+
+export default function Leaderboard() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Leaderboard 🐋</h1>
+      <LeaderboardTable />
+    </div>
+  )
+}
