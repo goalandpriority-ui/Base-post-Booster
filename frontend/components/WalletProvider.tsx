ae226ccf7e9cc0e6a48980980575ffb1ee5dfc88
goalandpriority-ui <goalandpriority@gmail.com>
2026-02-25 20:51:30 +0530

    Update WalletProvider.tsx

--- frontend/components/WalletProvider.tsx
+++ frontend/components/WalletProvider.tsx
@@ -1,23 +1,9 @@
 "use client"
 
 import { ReactNode } from "react"
-import { configureChains, createConfig, WagmiConfig } from "wagmi"
-import { base, goerli } from "wagmi/chains"
-import { publicProvider } from "wagmi/providers/public"
-import { MetaMaskConnector } from "wagmi/connectors/metaMask"
+import { WagmiConfig } from "wagmi"
+import { wagmiConfig } from "../wagmiConfig"
 
-const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [base, goerli],
-  [publicProvider()]
-)
-
-const wagmiConfig = createConfig({
-  autoConnect: true,
-  connectors: [new MetaMaskConnector({ chains })],
-  publicClient,
-  webSocketPublicClient,
-})
-
-export function WalletProvider({ children }: { children: ReactNode }) {
+export default function WalletProvider({ children }: { children: ReactNode }) {
   return <WagmiConfig config={wagmiConfig}>{children}</WagmiConfig>
 }
