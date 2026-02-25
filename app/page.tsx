2110d1682798b34b6d3977fd7402b997e45ec752
goalandpriority-ui <goalandpriority@gmail.com>
2026-02-25 12:46:20 +0530

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import BoostForm from "@/components/BoostForm"
+
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Base Post Booster 🚀</h1>
+      <BoostForm />
+    </div>
+  )
+}
