e4db4cae03dd62e43e8868f6409911b362ab493c
goalandpriority-ui <goalandpriority@gmail.com>
2026-02-26 14:32:44 +0530

    Delete frontend/components/WalletProvider.tsx

--- frontend/components/WalletProvider.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import { PropsWithChildren } from 'react'
-import { WagmiConfig } from 'wagmi'
-import { wagmiConfig } from '../wagmiConfig'
-
-export function WalletProvider({ children }: PropsWithChildren) {
-  return <WagmiConfig config={wagmiConfig}>{children}</WagmiConfig>
-}
