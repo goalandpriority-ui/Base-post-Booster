d8c5bfd773e66e487411010969707262bfd15d75
goalandpriority-ui <goalandpriority@gmail.com>
2026-02-28 06:52:38 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
-/** @type {import('next').NextConfig} */
+/** next.config.js */
 const nextConfig = {
   async headers() {
     return [
       {
         source: '/(.*)',
         headers: [
-          { key: 'X-Frame-Options', value: 'ALLOWALL' }, 
+          { key: 'X-Frame-Options', value: 'ALLOWALL' },
           { key: 'Content-Security-Policy', value: "frame-ancestors 'self' https://*.farcaster.xyz" },
         ],
       },
