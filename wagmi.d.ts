4e107fe95c0844c458aee4677609f831cff05ce0
goalandpriority-ui <goalandpriority@gmail.com>
2026-03-02 13:32:30 +0530

    Update wagmi.d.ts

--- wagmi.d.ts
+++ wagmi.d.ts
@@ -2,6 +2,6 @@ import { type Config } from 'wagmi'
 
 declare module 'wagmi' {
   interface Register {
-    config: Config  // or typeof config if you export config from here
+    config: Config
   }
 }
