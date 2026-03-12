cd8a70f09c88c2ad3790ee93d0ad8d42e1668bca
goalandpriority-ui <goalandpriority@gmail.com>
2026-03-12 16:51:19 +0530

    Create providers.tsx

---
+++ app/providers.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { WagmiProvider } from "wagmi"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactNode } from "react"
+import { wagmiConfig } from "./wagmiConfig"
+
+const queryClient = new QueryClient()
+
+export default function Providers({ children }: { children: ReactNode }) {
+  return (
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
+    </WagmiProvider>
+  )
+}
