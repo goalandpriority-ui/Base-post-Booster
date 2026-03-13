eed8d528f10a3634ac5ddd5001616c96217b2dea
goalandpriority-ui <goalandpriority@gmail.com>
2026-03-13 16:57:55 +0530

    Update route.ts

--- app/api/wars/route.ts
+++ app/api/wars/route.ts
@@ -1,21 +1,34 @@
 import { NextResponse } from "next/server"
 import { prisma } from "@/lib/prisma"
 
+export const dynamic = "force-dynamic"
+
 export async function GET() {
 
   try {
 
     const tokens = await prisma.boost.groupBy({
+
       by: ["contract"],
+
+      where: {
+        contract: {
+          not: ""
+        }
+      },
+
       _count: {
         contract: true
       },
+
       orderBy: {
         _count: {
           contract: "desc"
         }
       },
+
       take: 10
+
     })
 
     return NextResponse.json(tokens)
